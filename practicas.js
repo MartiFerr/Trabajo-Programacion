de38c292f175efdd9a39cee2ea90e04107b11da3
MartiFerr <136343935+MartiFerr@users.noreply.github.com>
2023-08-30 16:40:03 -0300

    cambio

---
+++ practicas.js
@@ -0,0 +1,76 @@
+/* 1) Crear una funcion que calcule el cuadrado de un numero
+2) Crear una funcion que calcule el cubo de un numero 
+3) Crer una funcion por cada de las siguientes operaciones:
+a) x^2-x-10
+b) (x-3)*2
+c) x*x^2+20
+d) (x/2)+5
+e) x^3+x^2+10
+f) (x*10)*5
+*/
+
+function Cuadrado(b,b)
+{
+let Cuadrado= b * b
+return Cuadrado
+}
+alert(Cuadrado(5,5))
+
+
+
+function Cubo(b,b,b)
+{
+let Cubo = b * b * b
+return Cubo
+}
+alert(Cubo(8,8,8))
+
+
+function Suma(x,x)
+{
+let Suma = (x * 2) - (x - 10)
+return Suma
+}
+alert(Suma (2,4))
+
+
+function Multiplicacion (x)
+{
+let Multiplicacion = (x-3)*2
+return Multiplicacion
+}
+alert(Multiplicacion (5))
+
+
+function MultiplicacionX2 (x)
+{
+let MultiplicacionX2 = x*x^2+20
+return MultiplicacionX2
+}
+alert(MultiplicacionX2 (2))
+
+
+function Divicion (x)
+{
+let Divicion = (x/2)+5
+return Divicion
+}
+alert(Divicion(10))
+
+
+function Cuenta (x)
+{
+let Cuenta =x^3+x^2+10 
+return Cuenta
+}
+alert(Cuenta(4))
+
+
+
+function Resta(x)
+{
+let Resta = (x*10)*5
+return Resta
+}
+alert(Resta(6))
+
